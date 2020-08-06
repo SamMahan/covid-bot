16ed6e402e2135b052501af149bbae665046ae30
SamMahan <sfmahan@gmail.com>
2020-08-05 20:38:23 -0400

    bup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -105,6 +105,7 @@ query the API for data on that country. \n
     private function checkCommand($searchText, $message)
     {
         $chat = $message['chat'];
+        error_log(print_r($chat, true));
         $append = ($chat['type'] === 'private') ? '' : '@' . env('BOT_NAME', 'covid_watch_bot');
         $searchText = $searchText . $append;
         if (str_contains($message['text'], $searchText)) return true;
